51c8352882fb91c11744cf8aeba0573f71410d93
David <david.yang026@gmail.com>
2024-10-07 16:59:25 -0700

    font awesome setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 const App = () => {
-  return <h1 className="font-playfair">Hello</h1>
+  return <div>
+    <h1 className="font-playfair">Hello</h1>
+    <i className="fa-solid fa-face-awesome"></i>
+    </div>
 }
 
 export default App;
