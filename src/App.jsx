02a9f730796b72005e1de5b05c5b9c8cbb7f85cf
David <david.yang026@gmail.com>
2024-10-07 17:07:19 -0700

    react router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignInPage from "./pages/auth/SignInPage";
+import SignUpPage from "./pages/auth/SignUpPage";
+
 const App = () => {
-  return <div>
-    <h1 className="font-playfair">Hello</h1>
-    <i className="fa-solid fa-face-awesome"></i>
-    </div>
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
