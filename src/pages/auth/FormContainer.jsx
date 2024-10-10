04e61b77a2dfeb215f95223273c65973d01ecb2e
David <david.yang026@gmail.com>
2024-10-09 22:20:00 -0700

    refactored

--- src/pages/auth/FormContainer.jsx
+++ src/pages/auth/FormContainer.jsx
@@ -1,11 +1,25 @@
 const FormContainer = (props) => {
   const { children } = props;
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <img src="https://static-task-assets.react-formula.com/capstone_logo_dark.png" 
-      className="w-24"/>
-      <div className="my-2 text-2xl text-emerald-800 font-bold font-playfair">Plant Depot</div>
-      {children}
+    <div className="flex w-full">
+      <div className="hidden md:flex relative">
+        <img
+          src="https://static-task-assets.react-formula.com/capstone_sign_in_scene.png"
+          className="h-screen object-cover"
+        />
+        <div className="absolute w-full h-full bg-black/20"></div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center flex-1 h-screen px-5">
+        <img
+          src="https://static-task-assets.react-formula.com/capstone_logo_dark.png"
+          className="w-24"
+        />
+        <div className="my-2 text-2xl text-emerald-800 font-bold font-playfair">
+          Plant Depot
+        </div>
+        {children}
+      </div>
     </div>
   );
 };
--- src/pages/auth/SignInPage.jsx
+++ src/pages/auth/SignInPage.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const SignInPage = () => {
   return (
-    <div className="flex justify-center bg-emerald-50">
+    <div className="flex bg-emerald-50">
       <FormContainer>
         <AuthForm
           fields={[
