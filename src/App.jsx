e225ecc3d16fd54dea63e6088c85cc0e3fbcdf6c
David <david.yang026@gmail.com>
2024-10-10 23:01:59 -0700

    API integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignInPage from "./pages/auth/SignInPage";
 import SignUpPage from "./pages/auth/SignUpPage";
+import apiFetch from "./services/apiFetch"
+
 
 const App = () => {
   return (
