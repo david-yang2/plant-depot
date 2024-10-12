7ca34597f728550941a8a3a0e075dfaf019fa2cc
David <david.yang026@gmail.com>
2024-10-12 09:34:43 -0700

    server side validation

--- src/pages/auth/SignUpPage.jsx
+++ src/pages/auth/SignUpPage.jsx
@@ -2,11 +2,14 @@ import AuthForm from "./AuthForm";
 import FormContainer from "./FormContainer";
 import { Link } from "react-router-dom";
 import * as userServices from "../../services/user"
+import {useState} from "react"
 
 const SignUpPage = () => {
+  const [error, setError] = useState("")
   return (
     <div className="flex justify-center bg-emerald-50">
       <FormContainer>
+        <div className="text-red-600">{error}</div>
         <AuthForm
           fields={[
             {
@@ -21,18 +24,20 @@ const SignUpPage = () => {
           ]}
           buttonLabel="Create"
           onSubmit={async (values) => {
+
+            // client side validation
             if (values.username.length < 4) {
-              console.log("username is too short")
+              setError("username is too short")
               return;
             }
 
             if (values.password.length <4 ) {
-              console.log("password is too short")
+              setError("password is too short")
               return;
             }
 
             if (values.password !== values["confirm password"]) {
-              console.log("passwords do not match")
+              setError("passwords do not match")
               return;
             }
 
@@ -42,6 +47,15 @@ const SignUpPage = () => {
               username: values.username,
               password: values.password
             })
+
+            // server side validation
+
+            if (response.status === 201){
+              setError("User Created")
+            } else {
+              const data = await response.json();
+              setError(data.error)
+            }
 
             console.log(response.status)
           }}
