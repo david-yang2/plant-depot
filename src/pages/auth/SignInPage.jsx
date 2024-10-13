7a09003e9d4c60a1137c8802c3e4aec15c49b716
David <david.yang026@gmail.com>
2024-10-13 07:40:28 -0700

    creating a session and handling errors

--- src/pages/auth/SignInPage.jsx
+++ src/pages/auth/SignInPage.jsx
@@ -1,11 +1,15 @@
 import AuthForm from "./AuthForm";
 import FormContainer from "./FormContainer";
 import { Link } from "react-router-dom";
+import * as userService from "services/user"
+import {useState} from "react"
 
 const SignInPage = () => {
+  const [error, setError] = useState("")
   return (
     <div className="flex bg-emerald-50">
       <FormContainer>
+      <div className="text-red-600">{error}</div>
         <AuthForm
           fields={[
             { label: "username", type: "text" },
@@ -15,6 +19,20 @@ const SignInPage = () => {
             },
           ]}
           buttonLabel="Sign In"
+          onSubmit={async (values) => {
+            const response = await userService.createSession({
+              username: values.username,
+              password: values.password
+            })
+
+            if (response.status === 201) {
+              console.log("You've logged in successfully")
+              setError("")
+            } else {
+              const data = await response.json()
+              setError(data.error)
+            }
+          }}
         />
         <Link to="/sign-up" className="text-emerald-700 underline mt-3">
           Create an Account
--- src/services/user.js
+++ src/services/user.js
@@ -1,8 +1,15 @@
-import apiFetch from "./apiFetch"
+import apiFetch from "./apiFetch";
 
 // POST - /users
 //   body: { username, password }
-export const createUser = ({username, password}) => apiFetch("POST", "/users", {
+export const createUser = ({ username, password }) =>
+  apiFetch("POST", "/users", {
     username,
-    password
-})
+    password,
+  });
+
+// POST - /users/session
+//   body: { username, password }
+
+export const createSession = ({ username, password }) =>
+  apiFetch("POST", "/users/session", { username, password });
